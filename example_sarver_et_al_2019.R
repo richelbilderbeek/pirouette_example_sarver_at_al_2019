e3acdaf2521d5d955110349e805d4fb8cd767432
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-15 11:46:35 +0100

    EOD

--- example_sarver_et_al_2019.R
+++ example_sarver_et_al_2019.R
@@ -50,10 +50,14 @@ for (i in seq_along(experiments)) {
   experiments[[i]]$beast2_options$rng_seed <- rng_seed
 }
 
-# Testing
-if (1 == 1) {
+# Shorter on Travis
+if (is_on_travis()) {
   for (i in seq_along(experiments)) {
-    experiments[[i]]$inference_model$mcmc <- create_mcmc(chain_length = 20000, store_every = 1000)
+    experiments[[i]]$inference_model$mcmc$chain_length <- 3000
+    experiments[[i]]$inference_model$mcmc$store_every <- 1000
+    #experiments[[i]]$est_evidence_mcmc$chain_length <- 3000
+    #experiments[[i]]$est_evidence_mcmc$store_every <- 1000
+    #experiments[[i]]$est_evidence_mcmc$epsilon <- 100.0
   }
 }
 

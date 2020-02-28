387d38e2e95153e395afbce824117c0ffdb9086c
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 13:10:07 +0100

    EOD

--- example_sarver_et_al_2019.R
+++ example_sarver_et_al_2019.R
@@ -10,9 +10,9 @@ is_testing <- is_on_travis()
 root_folder <- getwd()
 example_no <- "sarver_et_al_2019" # Not exactly a number
 rng_seed <- 314
-example_folder <- file.path(root_folder, paste0("example_", example_no, "_", rng_seed))
-dir.create(example_folder, showWarnings = FALSE, recursive = TRUE)
-setwd(example_folder)
+folder_name <- file.path(root_folder, paste0("example_", example_no, "_", rng_seed))
+dir.create(folder_name, showWarnings = FALSE, recursive = TRUE)
+setwd(folder_name)
 set.seed(rng_seed)
 testit::assert(is_beast2_installed())
 
@@ -29,31 +29,8 @@ phylogeny  <- TreeSim::sim.bd.taxa.age(
 
 ape::write.tree(phylogeny, file = "tree_true.fas")
 
-alignment_params <- create_alignment_params(
-  sim_tral_fun = get_sim_tral_with_std_nsm_fun(
-    mutation_rate = 0.1
-  ),
-  root_sequence = create_blocked_dna(length = 1000)
-)
-
-# JC69, strict, Yule
-generative_experiment <- create_gen_experiment()
-check_experiment(generative_experiment)
-
-experiments <- list(generative_experiment)
-
-twinning_params <- create_twinning_params(
-  sim_twin_tree_fun = get_sim_bd_twin_tree_fun(),
-  sim_twal_fun = get_sim_twal_same_n_muts_fun(
-    mutation_rate = 0.1,
-    max_n_tries = 1000
-  )
-)
-
-pir_params <- create_pir_params(
-  alignment_params = alignment_params,
-  experiments = experiments,
-  twinning_params = twinning_params
+pir_params <- create_std_pir_params(
+  folder_name = folder_name
 )
 
 # Shorter on Travis
@@ -68,20 +45,20 @@ errors <- pir_run(
 
 utils::write.csv(
   x = errors,
-  file = file.path(example_folder, "errors.csv"),
+  file = file.path(folder_name, "errors.csv"),
   row.names = FALSE
 )
 
 pir_plot(errors) +
-  ggsave(file.path(example_folder, "errors.png"))
+  ggsave(file.path(folder_name, "errors.png"))
 
 pir_to_pics(
   phylogeny = phylogeny,
   pir_params = pir_params,
-  folder = example_folder
+  folder = folder_name
 )
 
 pir_to_tables(
   pir_params = pir_params,
-  folder = example_folder
+  folder = folder_name
 )

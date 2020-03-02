45acd09b39897e9777ac7449632f8a546502f384
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-02 13:48:06 +0100

    Use of 'pir_save'

--- example_sarver_et_al_2019.R
+++ example_sarver_et_al_2019.R
@@ -1,17 +1,14 @@
+# pirouette example that replicates Sarver et al., 2019 
+# (not done yet)
 library(pirouette)
 library(beautier)
-
-################################################################################
 # Constants
-################################################################################
 is_testing <- is_on_travis()
 example_no <- "sarver_et_al_2019" # Not exactly a number
 rng_seed <- 314
 folder_name <- paste0("example_", example_no, "_", rng_seed)
 
-################################################################################
 # Create phylogeny
-################################################################################
 set.seed(rng_seed)
 # Same as https://github.com/bricesarver/prior_simulation_study/blob/master/simulate_trees.R#L26
 phylogeny  <- TreeSim::sim.bd.taxa.age(
@@ -23,31 +20,21 @@ phylogeny  <- TreeSim::sim.bd.taxa.age(
   frac = 1.0,
   mrca = FALSE
 )[[1]]
-
-ape::write.tree(phylogeny, file = "tree_true.fas")
-
-################################################################################
 # Setup pirouette
-################################################################################
 pir_params <- create_std_pir_params(
   folder_name = folder_name
 )
-
 if (is_testing) {
   pir_params <- shorten_pir_params(pir_params)
 }
 
-################################################################################
 # Run pirouette
-################################################################################
 pir_out <- pir_run(
   phylogeny,
   pir_params = pir_params
 )
 
-################################################################################
 # Save results
-################################################################################
 pir_save(
   phylogeny = phylogeny,
   pir_params = pir_params,
